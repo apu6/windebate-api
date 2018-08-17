3aa0b6a69b619b8f76df4d4cb51ca32b2c917b2e
Amy Pu <amy_pu@brown.edu>
2018-08-16 23:35:04 -0400

    trying error handling again

--- __init__.py
+++ __init__.py
@@ -161,7 +161,7 @@ def menteeLogin():
 
     if request.method == 'POST':
         req = request.get_json()
-        if req == None:
+        if len(req) == 0:
             raise NotFound()
 
         row = 1
